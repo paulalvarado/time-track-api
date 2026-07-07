b3384615acfbec676a08d4b4872698f1cdd9bd0b
Paúl Pérez <paulperezsv@gmail.com>
2026-07-07 15:53:52 -0600

    fix(cors): agregar propiedad \ con estructura esperada por CorsService nativo

--- app/Config/Cors.php
+++ app/Config/Cors.php
@@ -6,9 +6,12 @@ use CodeIgniter\Config\BaseConfig;
 
 class Cors extends BaseConfig
 {
-    public array $allowedOrigins = ['http://localhost:5173', 'http://localhost:5172', 'https://time-track-app.paulperez.dev'];
-    public array $allowedHeaders = ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-TOKEN'];
-    public array $allowedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'];
-    public bool $allowCredentials = true;
-    public int $maxAge = 86400;
+    public array $default = [
+        'allowedOrigins'         => ['http://localhost:5173', 'http://localhost:5172', 'https://time-track-app.paulperez.dev'],
+        'allowedOriginsPatterns' => [],
+        'supportsCredentials'    => true,
+        'allowedHeaders'         => ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-TOKEN'],
+        'allowedMethods'         => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+        'maxAge'                 => 86400,
+    ];
 }
